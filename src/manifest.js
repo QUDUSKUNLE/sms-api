f360bd615a37d8b34990868dd013f7de0c550f9b
temmyogunbo <Ogunbotemilola@yahoo.com>
2018-12-10 11:19:50 +0100

    add manifest file

---
+++ src/manifest.js
@@ -0,0 +1,53 @@
+import hapiBodyparser from 'hapi-bodyparser';
+import hapiPino from 'hapi-pino';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const manifest = {
+  server: {
+    port: process.env.PORT,
+  },
+  register: {
+    plugins: [
+      {
+        plugin: hapiBodyparser,
+        options: {
+          parser: { allowDots: true, strictNullHandling: true },
+          sanitizer: {
+            trim: true,
+            stripNullorEmpty: true,
+          },
+          merge: false,
+          body: false,
+        },
+      },
+      {
+        plugin: hapiPino,
+        options: {
+          prettyPrint: true,
+          logEvents: ['response', 'onPostStart'],
+        },
+      },
+      {
+        plugin: './routes/messages',
+        routes: {
+          prefix: '/api/v1',
+        },
+      },
+      {
+        plugin: './routes/contacts',
+        routes: {
+          prefix: '/api/v1',
+        },
+      },
+    ],
+    options: {
+      once: true,
+    },
+  },
+};
+
+export const options = {
+  relativeTo: __dirname,
+};
